6048f6d91390578bde4bee8777be4673c981570f
iLohas <iloilo_has@yahoo.co.jp>
2016-07-18 16:12:01 +0900

    微修正

--- src/application/controllers/IndexController.php
+++ src/application/controllers/IndexController.php
@@ -26,32 +26,4 @@ extends
 	 */
 	public function indexAction() {
 		$this->_log->debug(__CLASS__ . ":" . __FUNCTION__ . " called:(" . __LINE__ . ")");
-		$chk	= Auth::loginCheck();
-		if ($chk != false) {
-			$this->_log->debug('ログインしています．');
-			$this->redirect('/item');
-			return ;
-		}
-		
-		/** セッション情報が無い場合 **/
-		$params		= $this->getPostList();
-		if (count($params) == 0) {
-			$this->_log->debug("パラメータがPOSTされていません．");
-			return ;
-		}
-		
-		$chechItem	= array(
-				"email"		=> "NotNull",
-				"password"	=> "NotNull"
-		);
-		
-		$mapper	= new Index();
-		$result	= $mapper->login($params);
-		if ($result === true) {
-			$this->_log->debug("ログイン失敗．");
-			return ;
-		}
-
-		$this->redirect('/item');
-	}
 }
